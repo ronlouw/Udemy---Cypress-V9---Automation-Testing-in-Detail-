9cc2d983fcec25e2f348647ac618f0cca44791b2
Ronald van der Louw <r.van.der.louw@gmail.com>
2022-03-22 20:57:34 +0100

    add assertions to contact us pages

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -9,13 +9,15 @@ describe('Test Contact Us form via webdriverUni', () => {
     cy.get('[name="email"]').type('ronald@email.com')
     cy.get('textarea.feedback-input').type('Some feedback in this input')
     cy.get('[type="submit"]').click()
+    cy.get('h1').should('have.text', 'Thank You for your Message!')
   })
 
-  it.only('Should not be able to submit a succesfull submission via contact us form as all fields are required', () => {
+  it('Should not be able to submit a succesfull submission via contact us form as all fields are required', () => {
     cy.visit('https://www.webdriveruniversity.com/Contact-Us/contactus.html')
     cy.get('[name="first_name"]').type('Tom')
     cy.get('[name="last_name"]').type('Blogs')
     cy.get('textarea.feedback-input').type('Some feedback in this input')
     cy.get('[type="submit"]').click()
+    cy.get('body').contains('Error: all fields are required')
   })
 })
