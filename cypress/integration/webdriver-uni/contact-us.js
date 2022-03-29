7d48fd22257967705936d6c445888d79a00d3933
Ronald van der Louw <r.van.der.louw@gmail.com>
2022-03-29 21:03:51 +0200

    add asserts
    add some asserts on the document, title and url

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -3,6 +3,9 @@
 describe('Test Contact Us form via webdriverUni', () => {
   it('Should be able to submit a succesfull submission via contact us form', () => {
     cy.visit('https://www.webdriveruniversity.com/Contact-Us/contactus.html')
+    cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+    cy.title().should('include', 'WebDriver | Contact Us')
+    cy.url().should('include', 'contactus')
     // cy.get('#contact-us').click({ force: true })
     cy.get('[name="first_name"]').type('Ronald')
     cy.get('[name="last_name"]').type('van der Louw')
