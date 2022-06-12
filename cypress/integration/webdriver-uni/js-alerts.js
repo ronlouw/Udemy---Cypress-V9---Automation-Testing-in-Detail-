ef425480525a6620636db8397aaf767246d977a2
Ronald van der Louw <r.van.der.louw@gmail.com>
2022-06-12 10:43:36 +0200

    add test with stub

--- cypress/integration/webdriver-uni/js-alerts.js
+++ cypress/integration/webdriver-uni/js-alerts.js
@@ -24,4 +24,36 @@ describe('Handle js alerts', () => {
 
     cy.get('#confirm-alert-text').contains('You pressed OK!')
   })
+
+  it(' Validates js confirm alert box works correctly when clicking cancel', () => {
+    cy.visit('https://www.webdriveruniversity.com')
+    cy.get('#popup-alerts').invoke('removeAttr', 'target').click({ force: true })
+
+    cy.get('#button4').click()
+
+    cy.on('window:confirm', (str) => {
+      return false
+    })
+    cy.get('#confirm-alert-text').contains('You pressed Cancel!')
+  })
+
+  it.only(' Validates js confirm alert box works using a stub', () => {
+    cy.visit('https://www.webdriveruniversity.com')
+    cy.get('#popup-alerts').invoke('removeAttr', 'target').click({ force: true })
+
+    const stub = cy.stub()
+    cy.on('window:confirm', stub)
+
+    cy.get('#button4')
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith('Press a button!')
+      })
+      .then(() => {
+        return true
+      })
+      .then(() => {
+        cy.get('#confirm-alert-text').contains('You pressed OK!')
+      })
+  })
 })
